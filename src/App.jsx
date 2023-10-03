0d593c081aaaa4867a0ff39db37403083944ab9a
Ketan Tripathi <ketantripathi807@gmail.com>
2023-10-04 00:58:39 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="relative z-0 bg-black">
         <Timeline />
       </div>
---
+++ src/components/navscript.js
@@ -0,0 +1,38 @@
+let navlist = document.getElementById("navlist");
+let hamburger = document.getElementById("hamburger")
+function hide()
+{
+    hamburger.classList.toggle("cross");
+    if (navlist.style.display=='none') {
+        navlist.style.display ='block'
+    } else {
+        navlist.style.display ='none'
+    }
+    // if (hamburger.class) {
+        
+    // }
+}
+// const myNav = document.getElementById('mynav');
+// window.onscroll = function () { 
+//     if (document.body.scrollTop >= 50 ) {
+//         // myNav.classList.add("nav-colored");
+//         // myNav.classList.remove("nav-transparent");
+//         myNav.style.backgroundColor = "rgb(255,255,255,1)"
+//     } 
+//     // else {
+//     //     // myNav.classList.add("nav-transparent");
+//     //     // myNav.classList.remove("nav-colored");
+//     // }
+// };
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 1) {
+          $(".nav").css("background" , "white");
+        }
+  
+        else{
+            $(".nav").css("background" , "#333");  	
+        }
+    })
+})
