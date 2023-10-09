a060048f3dd45bc8846fd7ea7732902de7d9902f
Coolsheru2526 <shreyanshmishra.4365@gmail.com>
2023-10-09 20:34:23 +0530

    lined up properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
-
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Sponser from "./components/Sponser";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-black">
-        <Hero/>
-        <Timeline />
-      </div>
-      <Footer />
+       <div className="relative z-0 bg-black">
+         <Timeline />
+       </div>
+       <Navbar/>
+       <Hero/>
+       <About/>
+       <Sponser/>
+       <Footer/>
     </BrowserRouter>
   );
 };
