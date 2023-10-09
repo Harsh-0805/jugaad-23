bb996ef1f710e9d3f0642a7ea5bb8d37f2551a3c
Coolsheru2526 <shreyanshmishra.4365@gmail.com>
2023-10-09 20:07:29 +0530

    hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-black">
+        <section>
+          <Hero/>
+        </section>
         <Timeline />
       </div>
       <Footer />
