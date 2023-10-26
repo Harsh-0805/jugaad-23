0ef67ee870d36c5350aa6a9f4b4c68f029ce7800
Harsh-0805 <harshbadala0805@gmail.com>
2023-10-26 17:33:57 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Sponser from "./components/Sponsor";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
+import Register from "./components/Register";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Sponser />
+      <Register />
       <Contact />
       <Footer />
     </BrowserRouter>
