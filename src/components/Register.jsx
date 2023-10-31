fc05b7b8be2c1a6cba7f3fb0457eb1e2adab7184
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2023-10-31 16:21:40 +0530

    isloading added to registration

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -261,7 +261,7 @@ function Register() {
         <button
           type="submit"
           href="https://flowbite.com/docs/forms/select/"
-          disabled={!isChecked}
+          disabled={!isChecked && isLoading}
           class="rounded-md px-3.5 max-w-[170px] py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#000] text-white"
         >
           <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[orange] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
